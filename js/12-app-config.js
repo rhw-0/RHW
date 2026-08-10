ae94a3b1b3bbc0971ed649b760f5f50347e263dd
rhw-0 <ax.gro90@gmail.com>
2026-08-10 23:56:10 +0200

    Serve COMMS logo from RHW GitHub asset

--- js/12-app-config.js
+++ js/12-app-config.js
@@ -28,7 +28,7 @@ const RHW_APP_CONFIG = Object.freeze({
     })
   }),
   forum: Object.freeze({
-    logoUrl: 'https://i.imgur.com/TFXQ1So.png',
+    logoUrl: 'https://raw.githubusercontent.com/rhw-0/RHW/e935fd950906491b5128859c06dd0c62149db91e/assets/rhw-forum-logo.png',
     organisation: 'RESOLUTION HEAVY WORKS',
     subline: 'INDUSTRIAL MANUFACTURING HUB | NEW LONDON',
     brandColor: '#d4af37',
